22c6921a3e4947f26d0f3ccaedcfafbfd653f80e
runyu <wangrunyu010@gmail.com>
2021-03-18 12:10:43 +0800

    add read parquet file, add print schema

--- App/ReadParquet.py
+++ App/ReadParquet.py
@@ -23,15 +23,18 @@ parquetFile = read_parquet_file(spark, hdfs_host+hdfs_root_path+safe_entry_file_
 parquetFile.createOrReplaceTempView("safeEntryParquetFile")
 safe_entry_df = spark.sql("SELECT * FROM safeEntryParquetFile")
 safe_entry_df.show()
+safe_entry_df.printSchema()
 
 # Step 2: read resident parquet file
 parquetFile = read_parquet_file(spark, hdfs_host+hdfs_root_path+resident_file_dest)
 parquetFile.createOrReplaceTempView("residentParquetFile")
 resident_df = spark.sql("SELECT * FROM residentParquetFile")
 resident_df.show()
+resident_df.printSchema()
 
 # Step 3: read place parquet file
 parquetFile = read_parquet_file(spark, hdfs_host+hdfs_root_path+place_file_dest)
 parquetFile.createOrReplaceTempView("placeParquetFile")
 place_df = spark.sql("SELECT * FROM placeParquetFile")
 place_df.show()
+place_df.printSchema()
