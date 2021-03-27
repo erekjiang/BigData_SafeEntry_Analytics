888977a459298a8b293cb97799ea240b09c4b572
Di Jing <awerdj93@hotmail.com>
2021-03-27 17:13:26 +0800

    add lat long

--- EBDA_Source_Data_Simulation/Generate_Place.py
+++ EBDA_Source_Data_Simulation/Generate_Place.py
@@ -5,9 +5,40 @@ Created on Wed Mar 10 14:05:48 2021
 @author: cmbsguser
 """
 
-import pandas as pd
 import random
-import string
+import requests
+import sys
+import time
+from datetime import datetime
+
+import pandas as pd
+
+def pcode_to_data(pcode):
+    page = 1
+    results = []
+    retry_cnt = 0
+    while True:
+        try:
+            response = requests.get(
+                'https://developers.onemap.sg/commonapi/search?searchVal=%s&returnGeom=Y&getAddrDetails=Y&pageNum=%d' %
+                (pcode, page)).json()
+            results = results + response['results']
+            if response['totalNumPages'] > page:
+                page = page + 1
+            else:
+                break
+        except:
+            if retry_cnt > 10:
+                print('Fetching %s failed for too many times. Skipping ...' % pcode, file=sys.stderr, flush=True)
+                break
+            else:
+                retry_cnt += 1
+                print('Fetching %s failed for %d times. Retrying in 5-10 sec ...' % (pcode, retry_cnt), file=sys.stderr,
+                      flush=True)
+                time.sleep(5 + 5 * random.random())
+                continue
+
+    return results
 
 # generate users
 df_place = pd.DataFrame(columns=["place_id","place_name","url","postal_code", "address", "lat", "lon", "place category"])
@@ -15,9 +46,9 @@ df_place = pd.DataFrame(columns=["place_id","place_name","url","postal_code", "a
 # Table - places (place id, place name, bit.ly url, postal code, 
 # address, lat & long, place category(Mall, MRT, Retailer, Shops))
 
-
 df_hc = pd.read_csv(".\\in\\hawker-centres\\list-of-government-markets-hawker-centres.csv")
-df_mcst = pd.read_csv(".\\in\\mcst\\management-corporation-strata-title.csv",encoding='ANSI')
+df_mcst = pd.read_csv(".\\in\\mcst\\management-corporation-strata-title.csv", encoding='ANSI')
+df_pc = pd.read_json(".\\in\\location\\singpostcode.json")
 
 # process hawker centers
 for i in range(0, df_hc.shape[0]):
@@ -28,12 +59,31 @@ for i in range(0, df_hc.shape[0]):
     
     postal_code = list_address[-1].strip()[2:-1]
 
-    df_place = df_place.append({'place_id': 'pid_' + str(i+1), 
-                                'place_name': place_name, 
-                                'postal_code': postal_code, 
-                                'address': address},
+    # retrieve lat long
+    postal = postal_code[0:6]
+    latitude = ''
+    longitude = ''
+    if postal.isnumeric():
+        df_location = df_pc.loc[df_pc['POSTAL'] == postal]
+        if df_location.empty:
+            location = pcode_to_data('%06d' % int(postal))
+            if len(location) > 0:
+                latitude = location[0]['LATITUDE']
+                longitude = location[0]['LONGITUDE']
+        else:
+            latitude = df_location.iloc[0]['LATITUDE']
+            longitude = df_location.iloc[0]['LONGITUDE']
+
+    df_place = df_place.append({'place_id': 'pid_' + str(i+1),
+                                'place_name': place_name,
+                                'postal_code': postal_code,
+                                'address': address,
+                                'last_update_dt': datetime.now(),
+                                'lat': latitude,
+                                'lon': longitude
+                                },
                                ignore_index=True)
-    
+
 size = df_place.shape[0]
 
 # process hawker center
@@ -50,18 +100,35 @@ df_mcst = df_mcst.reset_index(drop=True)
 
 for j in range(df_mcst.shape[0]):
     place_name = df_mcst['usr_devtname'][j]
-    
+
     address = df_mcst['devt_location'][j]
     list_address = address.split(" ")
-    
+
     postal_code = list_address[-1].strip()
 
-    df_place = df_place.append({'place_id': 'pid_' + str(j + size), 
-                                'place_name': place_name, 
-                                'postal_code': postal_code, 
-                                'address': address},
+    # retrieve lat long
+    postal = postal_code[0:6]
+    latitude = ''
+    longitude = ''
+    if postal.isnumeric():
+        df_location = df_pc.loc[df_pc['POSTAL'] == postal_code[0:6]]
+        if df_location.empty:
+            location = pcode_to_data('%06d' % int(postal_code[0:6]))
+            if len(location) > 0:
+                latitude = location[0]['LATITUDE']
+                longitude = location[0]['LONGITUDE']
+        else:
+            latitude = df_location.iloc[0]['LATITUDE']
+            longitude = df_location.iloc[0]['LONGITUDE']
+
+    df_place = df_place.append({'place_id': 'pid_' + str(j + size),
+                                'place_name': place_name,
+                                'postal_code': postal_code,
+                                'address': address,
+                                'last_update_dt': datetime.now(),
+                                'lat': latitude,
+                                'lon': longitude
+                                },
                                ignore_index=True)
     
-    
 df_place.to_csv('.\\out\\place.csv', index=False)
-
