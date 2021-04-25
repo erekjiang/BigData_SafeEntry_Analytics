92f1039638cf76b2695b601bd500644d879bd76f
runyu <wangrunyu010@gmail.com>
2021-04-26 00:15:52 +0800

    add df cache and shorten view hdfs file

--- App/view_hdfs_parquet.py
+++ App/view_hdfs_parquet.py
@@ -13,41 +13,33 @@ resident_file_dest = "resident.parquet"
 place_file_dest = "place.parquet"
 safe_entry_file_dest = "entry_record.parquet"
 case_file_dest = "case.parquet"
-contact_graph_edge_file_dest = "contact_graph_edge.parquet"
-contact_graph_vertex_file_dest = "contact_graph_vertex.parquet"
+case_daily_summary_file_dest = "case_daily_summary.parquet"
+
+safe_entry_export_dest = "hdfs_files/safe_entry"
+resident_export_dest ="hdfs_files/resident"
+place_export_dest ="hdfs_files/place"
+case_export_dest ="hdfs_files/case"
+case_daily_summary_export_dest ="hdfs_files/case_daily_summary"
+
+def view_export_hdfs_files(file_dest, export_dest):
+    df = read_parquet_file(spark, hdfs_host + hdfs_root_path + file_dest)
+    df.cache()
+    df.show()
+    df.printSchema()
+    df.coalesce(1).write.format("csv").option("header", "true").option("sep", ",").mode("overwrite").save(export_dest)
+    df.unpersist()
 
 # Step 1: read safe entry parquet file
-safe_entry_df  = read_parquet_file(spark, hdfs_host+hdfs_root_path+safe_entry_file_dest)
-safe_entry_df.show()
-safe_entry_df.printSchema()
-safe_entry_df.coalesce(1).write.format("csv").option("header","true").option("sep",",").mode("overwrite").save("hdfs_files/safe_entry")
+view_export_hdfs_files(safe_entry_file_dest, safe_entry_export_dest)
 
 # Step 2: read resident parquet file
-resident_df = read_parquet_file(spark, hdfs_host+hdfs_root_path+resident_file_dest)
-resident_df.show()
-resident_df.printSchema()
-resident_df.coalesce(1).write.format("csv").option("header","true").option("sep",",").mode("overwrite").save("hdfs_files/resident")
+view_export_hdfs_files(resident_file_dest, resident_export_dest)
 
 # Step 3: read place parquet file
-place_df = read_parquet_file(spark, hdfs_host+hdfs_root_path+place_file_dest)
-place_df.show()
-place_df.printSchema()
-place_df.coalesce(1).write.mode("overwrite").csv("hdfs_files/place")
+view_export_hdfs_files(place_file_dest, place_export_dest)
 
 # Step 4: read case parquet file
-case_df = read_parquet_file(spark, hdfs_host+hdfs_root_path+case_file_dest)
-case_df.show()
-case_df.printSchema()
-case_df.coalesce(1).write.format("csv").option("header","true").option("sep",",").mode("overwrite").save("hdfs_files/case")
+view_export_hdfs_files(case_file_dest, case_export_dest)
 
-# Step 5: read Contact Graph Edge parquet file
-contact_graph_edge_df= read_parquet_file(spark, hdfs_host+hdfs_root_path+contact_graph_edge_file_dest)
-contact_graph_edge_df.show()
-contact_graph_edge_df.printSchema()
-contact_graph_edge_df.coalesce(1).write.format("csv").option("header","true").option("sep",",").mode("overwrite").save("hdfs_files/contact_graph_edge")
-
-# Step 6: read Contact Graph Edge parquet file
-contact_graph_vertex_df = read_parquet_file(spark, hdfs_host+hdfs_root_path+contact_graph_vertex_file_dest)
-contact_graph_vertex_df.show()
-contact_graph_vertex_df.printSchema()
-contact_graph_vertex_df.coalesce(1).write.format("csv").option("header","true").option("sep",",").mode("overwrite").save("hdfs_files/contact_graph_vertex")
+# Step 5: read case daily summary parquet file
+view_export_hdfs_files(case_daily_summary_file_dest, case_daily_summary_export_dest)
