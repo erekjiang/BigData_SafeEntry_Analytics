4d87e9020ee82a86bff0bc4ed5e65bf673bf385d
erekJiang <erekjiang@gmail.com>
2021-04-30 11:03:15 +0800

    update minor

--- App/batch_data_ingest.py
+++ App/batch_data_ingest.py
@@ -56,64 +56,64 @@ def read_and_store_file(schema, file_path, file_dest, key):
     print(f"============saved: {file_dest} to hdfs============")
     df.unpersist()
 
-# Step 1: Read & Store resident file
-resident_schema = StructType([StructField("resident_id", StringType(), False),
-                              StructField("resident_name", StringType(), True),
-                              StructField("nric", StringType(), False),
-                              StructField("phone_number", StringType(), False),
-                              StructField("last_update_dt", TimestampType(), False)])
-key = 'nric'
-read_and_store_file(resident_schema,resident_file_path, resident_file_dest, key)
-
-# Step 2: Read & Store place file
-place_schema = StructType([StructField("place_id", StringType(), False),
-                           StructField("place_name", StringType(), True),
-                           StructField("url", StringType(), True),
-                           StructField("postal_code", StringType(), False),
-                           StructField("address", StringType(), False),
-                           StructField("lat", DoubleType(), True),
-                           StructField("lon", DoubleType(), True),
-                           StructField("place_category", StringType(), True),
-                           StructField("last_update_dt", TimestampType(), False)])
-key = 'place_id'
-read_and_store_file(place_schema, place_file_path,place_file_dest,key)
-
-# Step 3: Read & Store safe entry file
-safe_entry_schema = StructType([StructField("record_id", StringType(), False),
-                                StructField("resident_id", StringType(), False),
-                                StructField("place_id", StringType(), True),
-                                StructField("entry_time", TimestampType(), False),
-                                StructField("exit_time", TimestampType(), True),
-                                StructField("last_update_dt", TimestampType(), False)])
-key = 'record_id'
-read_and_store_file(safe_entry_schema,safe_entry_file_path,safe_entry_file_dest,key)
-
-
-# Step 4: Read & Store case file
-case_schema = StructType([StructField("caseId", StringType(), False),
-                          StructField("nric", StringType(), True),
-                          StructField("passType", StringType(), True),
-                          StructField("nationality", StringType(), True),
-                          StructField("race", StringType(), True),
-                          StructField("name", StringType(), True),
-                          StructField("birthDt", TimestampType(), True),
-                          StructField("age", StringType(), True),
-                          StructField("gender", StringType(), True),
-                          StructField("diagnosedDate", TimestampType(), True),
-                          StructField("active", StringType(), True),
-                          StructField("activeStatus", StringType(), True),
-                          StructField("importedCase", StringType(), True),
-                          StructField("importedFromCountry", StringType(), True),
-                          StructField("hospitalizedHospital", StringType(), True),
-                          StructField("admittedDt", TimestampType(), True),
-                          StructField("dischargedDt", TimestampType(), True),
-                          StructField("deceased", StringType(), True),
-                          StructField("deceasedDt", TimestampType(), True),
-                          StructField("createdDttm", TimestampType(), True),
-                          StructField("lastUpdatedDttm", TimestampType(), True)])
-
-key = 'caseId'
-read_and_store_file(case_schema,case_file_path,case_file_dest, key)
+# # Step 1: Read & Store resident file
+# resident_schema = StructType([StructField("resident_id", StringType(), False),
+#                               StructField("resident_name", StringType(), True),
+#                               StructField("nric", StringType(), False),
+#                               StructField("phone_number", StringType(), False),
+#                               StructField("last_update_dt", TimestampType(), False)])
+# key = 'nric'
+# read_and_store_file(resident_schema,resident_file_path, resident_file_dest, key)
+#
+# # Step 2: Read & Store place file
+# place_schema = StructType([StructField("place_id", StringType(), False),
+#                            StructField("place_name", StringType(), True),
+#                            StructField("url", StringType(), True),
+#                            StructField("postal_code", StringType(), False),
+#                            StructField("address", StringType(), False),
+#                            StructField("lat", DoubleType(), True),
+#                            StructField("lon", DoubleType(), True),
+#                            StructField("place_category", StringType(), True),
+#                            StructField("last_update_dt", TimestampType(), False)])
+# key = 'place_id'
+# read_and_store_file(place_schema, place_file_path,place_file_dest,key)
+#
+# # Step 3: Read & Store safe entry file
+# safe_entry_schema = StructType([StructField("record_id", StringType(), False),
+#                                 StructField("resident_id", StringType(), False),
+#                                 StructField("place_id", StringType(), True),
+#                                 StructField("entry_time", TimestampType(), False),
+#                                 StructField("exit_time", TimestampType(), True),
+#                                 StructField("last_update_dt", TimestampType(), False)])
+# key = 'record_id'
+# read_and_store_file(safe_entry_schema,safe_entry_file_path,safe_entry_file_dest,key)
+#
+#
+# # Step 4: Read & Store case file
+# case_schema = StructType([StructField("caseId", StringType(), False),
+#                           StructField("nric", StringType(), True),
+#                           StructField("passType", StringType(), True),
+#                           StructField("nationality", StringType(), True),
+#                           StructField("race", StringType(), True),
+#                           StructField("name", StringType(), True),
+#                           StructField("birthDt", TimestampType(), True),
+#                           StructField("age", StringType(), True),
+#                           StructField("gender", StringType(), True),
+#                           StructField("diagnosedDate", TimestampType(), True),
+#                           StructField("active", StringType(), True),
+#                           StructField("activeStatus", StringType(), True),
+#                           StructField("importedCase", StringType(), True),
+#                           StructField("importedFromCountry", StringType(), True),
+#                           StructField("hospitalizedHospital", StringType(), True),
+#                           StructField("admittedDt", TimestampType(), True),
+#                           StructField("dischargedDt", TimestampType(), True),
+#                           StructField("deceased", StringType(), True),
+#                           StructField("deceasedDt", TimestampType(), True),
+#                           StructField("createdDttm", TimestampType(), True),
+#                           StructField("lastUpdatedDttm", TimestampType(), True)])
+#
+# key = 'caseId'
+# read_and_store_file(case_schema,case_file_path,case_file_dest, key)
 
 # Step 5: Read & Store case daily summary file
 case_daily_schema = StructType([StructField("date", StringType(), False),
